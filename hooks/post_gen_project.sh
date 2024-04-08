ea4a53e6e210a2b4eadc44f450e9428f7989d6ce
Ryan Lerch <ryanlerch@users.noreply.github.com>
2024-04-09 09:18:07 +1000

    use main as default branchname

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -3,7 +3,7 @@
 set -e
 set -x
 
-git init .
+git init -b main .
 git commit --allow-empty -s -m "Initial empty commit"
 git add .
 git commit -s -m "Project creation from the cookiecutter template"
